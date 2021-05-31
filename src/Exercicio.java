8ba9af02b8b84a0d73e97f53277c7b5f280ae9b6
carolinasrc <carolinasoares.src@gmail.com>
2021-05-31 20:41:21 -0300

    test

---
+++ src/Exercicio.java
@@ -0,0 +1,32 @@
+import modelo.Lancamento;
+import service.LancamentoService;
+
+import java.util.List;
+
+import static java.lang.System.out;
+import static java.util.Comparator.comparing;
+
+public class Exercicio {
+
+	public static void main(String[] args) {
+		List<Lancamento> lancamentos = new LancamentoService().listarLancamentos();
+
+		out.println("Gastos ordenados por meses: ");
+		lancamentos.stream()
+				.sorted(comparing(Lancamento::getMes))
+				.forEach(v -> out.println(v.getValor()));
+
+		out.println("Todos os lancamentos da categoria 4: ");
+		lancamentos.forEach(lancamento -> {
+			lancamento.setCategoria(4);
+			out.printf(String.valueOf(lancamento));
+		});
+
+		out.println("");
+
+		out.println("Total da fatura do mes 5: ");
+		lancamentos.stream().sorted(comparing(Lancamento::getMes).reversed());
+		out.println(lancamentos.get(2).getValor()); // pegando mês por índice e exibindo o total da fatura
+
+	}
+}
