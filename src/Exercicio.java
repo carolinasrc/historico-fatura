68e9a3522fd4fef088f6ba6fcec6c4f44d9a5e81
carolinasrc <carolinasoares.src@gmail.com>
2021-06-01 10:31:04 -0300

    alguns ajustes feitos

--- src/Exercicio.java
+++ src/Exercicio.java
@@ -8,25 +8,23 @@ import static java.util.Comparator.comparing;
 
 public class Exercicio {
 
-	public static void main(String[] args) {
-		List<Lancamento> lancamentos = new LancamentoService().listarLancamentos();
+    public static void main(String[] args) {
+        List<Lancamento> lancamentos = new LancamentoService().listarLancamentos();
 
-		out.println("Gastos ordenados por meses: ");
-		lancamentos.stream()
-				.sorted(comparing(Lancamento::getMes))
-				.forEach(v -> out.println(v.getValor()));
+        out.println("Gastos ordenados por meses: ");
+        lancamentos.stream()
+                .sorted(comparing(Lancamento::getMes))
+                .forEach(v -> out.println(v.getValor()));
 
-		out.println("Todos os lancamentos da categoria 4: ");
-		lancamentos.forEach(lancamento -> {
-			lancamento.setCategoria(4);
-			out.printf(String.valueOf(lancamento));
-		});
+        out.println("\nTodos os lançamentos da categoria 4: ");
+        lancamentos.forEach(lancamento -> {
+            lancamento.setCategoria(4);
+            out.printf(String.valueOf(lancamento));
+        });
 
-		out.println("");
+        out.println("\nTotal da fatura do mês 5: ");
+        lancamentos.stream().sorted(comparing(Lancamento::getMes).reversed());
+        out.println(lancamentos.get(2).getValor()); // pegando mês por índice e exibindo o total da fatura
 
-		out.println("Total da fatura do mes 5: ");
-		lancamentos.stream().sorted(comparing(Lancamento::getMes).reversed());
-		out.println(lancamentos.get(2).getValor()); // pegando mês por índice e exibindo o total da fatura
-
-	}
+    }
 }
